fb6b6334492c6c5ac841aa70fc937076bbad8b94
olyislam <116255813+oly9@users.noreply.github.com>
2024-09-30 15:17:23 +0600

    Prepare APIs template in js

--- reactwebproject/src/App.js
+++ reactwebproject/src/App.js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-
+  
 useEffect(()=>{
   const handleMessage = (event) =>{
     console.log(event.data);
-    if(event.data.type === "UPDATE_ROTATESIGN"){
-      document.getElementById("rotateSign").innerHTML = "Rotate Sign:" + event.data.value;
-    }
+    ReceiveEventDataFromUnity(event.data.type, event.data.value);
   };
 
    window.addEventListener('message', handleMessage);
@@ -16,22 +14,25 @@ useEffect(()=>{
    }
 })
 
-
-
-
-
-const sendMessageToUnity = (value) => {
-  const iframe = document.querySelector('iframe');
-  if(iframe){
-    iframe.contentWindow.postMessage({type: 'CALL_UNITY_FUNCTION', value: value}, '*');
+const ReceiveEventDataFromUnity = (jsEventType, value) => {
+  if(jsEventType === "UPDATE_ROTATESIGN"){
+    document.getElementById("rotateSign").innerHTML = "Rotate Sign:" + value;
   }
 }
+
+const sendEventDataToUnity = (jsEventType, value) => {
+  const iframe = document.querySelector('iframe');
+  if(iframe){
+    iframe.contentWindow.postMessage({type: jsEventType, value: value}, '*');
+  }
+}
+
 
   return (
     <div className="App">
       <header className="App-header">
         <p> Welcome to React Unity Webgpu Sample Project </p>
-        <button onClick={()=> sendMessageToUnity(2.0)}> Multiply Rotate Speed</button>
+        <button onClick={()=> sendEventDataToUnity('CALL_UNITY_FUNCTION', 2.0)}> Multiply Rotate Speed</button>
         <p id="rotateSign">Rotate Sign: 0 </p>
         <iframe src="http://localhost/UnityWebBuild/" width={1000} height={500} ></iframe>
       </header>
